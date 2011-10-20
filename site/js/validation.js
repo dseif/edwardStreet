bd942d9e402a6d2c8c1f21590c76af2633dd230d
David Seifried <david.c.seifried@gmail.com>
2011-10-20 14:30:57 -0400

    Some more test validation stuff as well as a new layout for our validation javascript file

--- site/js/validation.js
+++ site/js/validation.js
@@ -7,72 +7,16 @@
 *
 */
 
-(function($) {
+(function() {
 
-var validation = function () {
+  window.edwardValidation = {
 
-	var rules = {
-	
-	email : {
-		check: function(value) {
-			if(value) {
-				return testPattern(value,"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])");
-			}
-			return true;
-		},
-		msg : "Email address is not valid"
-	},
-	required: {
-		check: function(value) {
-
-			if(value) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		},
-		msg : "A required field"
-	}
-}
-var testPattern = function(value, pattern) { //Private method
-
-	var regExp = new RegExp(pattern,"");
-	return regExp.test(value);
-}
-return {
-	addRule : function(name, rule) {
-	
-		rules[name] = rule;
-	},
-	getRule : function(name) {
-
-		return rules[name];
-	}
-	}
-}
-
-$.validation = new validation();
-})(jQuery);
-
-$.validation.addRule("test",{
-    check: function(value) {
-        if(value != "test") {
-            return false;
-        }
+    validateEmail: function( emailAddress ) {
+      if ( emailAddress !== "SASHA" ) {
+        return false;
+      } else {
         return true;
-    },
-    msg : "Must equal to the word test."
-});
-
-$.validation.addRule("xhr-test", {
-    check: function(value) {
-        $.get("/url-to-username-check", { username: value }, function(data) {
-            if(data.isValid) {
-                return true;
-            }
-            return false;
-        });
-    },
-    msg : "Username already exists."
-});
+      }
+    }
+  };
+})();
