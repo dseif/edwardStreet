578876abc22fd08328c5b779a26b93419e1a1c05
Sasha Atijas <satijas@learn.senecac.on.ca>
2011-10-23 15:18:42 -0400

    fixed html pages by removing w3c tag and adding a login link on top right

--- site/js/validation.js
+++ site/js/validation.js
@@ -3,7 +3,7 @@
 *	PRJ666 Edward Street
 *
 *	This validates forms
-*	Date:? 13-?Oct-2011
+*	Date: 13-Oct-2011
 *
 */
 
@@ -11,12 +11,30 @@
 
   window.edwardValidation = {
 
-    validateEmail: function( emailAddress ) {
-      if ( emailAddress !== "SASHA" ) {
+    validateGeneral: function( whatIsValidated ) {
+
+    }
+  };
+  
+  	var testPattern = function(value, pattern) { //Private method
+
+	var regExp = new RegExp(pattern,"");
+	return regExp.test(value);
+	}
+	
+    var validateEmail = function( emailAddress ) {
+      if (testPattern(emailAddress,"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])")) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+	
+	var validateRequired = function( required ) {
+      if (required == "") {
         return false;
       } else {
         return true;
       }
     }
-  };
 })();
